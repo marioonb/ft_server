c7faf5d8325a048b887756b085e69ab6c6c75a43
Marion BELORGEY <mbelorge@e1r10p16.42.fr>
2020-08-22 21:03:02 +0200

    samedi

--- src/essai.sh
+++ src/essai.sh
@@ -1,3 +1,5 @@
+
+#concernant NGINX
 mkdir -p var/www/localhost					#creait un dossier localhost
 mv ./default etc/mnginx/sites-available   #mmove default dans le dossier de config linux etc
 # le fcihier default se trouve dans etc/nginx/sites-enabled
@@ -7,8 +9,36 @@ mv ./default etc/mnginx/sites-available   #mmove default dans le dossier de conf
 #créer un nouveau fichier dans sites-available ;
 #ajouter un lien symbolique dans sites-enabled grâce à la commande ln.
 ln -s /etc/nginx/sites-available/default /etc/nginx/site-enabled
-chown -R $USER:$USER /var/www/
-chmod -R 755 /var/www/
+chown -R www-data /var/www/ # -R modifie tous ses sous-répertoires et ses sous-fichiers d'une manière récursive.
+chmod -R 755 /var/www/* #domer es droit ecr, lec, exe a tous les repertoire etc...
+# www-data est l'utilisateur que les serveurs Web sur Ubuntu (Apache, nginx, par exemple) utilisent par défaut
+#pour un fonctionnement normal. Le processus du serveur Web peut accéder à tous les fichiers auxquels www-data peut accéder.
+#Cela n'a pas d'autre importance.
+# 755 -> RWXR-XR-X
+
+#SSL
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key
+-out /etc/ssl/certs/nginx-selfsigned.crt < /etc/ssl.txt
+#* openssl *: Il s’agit de l’outil de ligne de commande de base pour la création et la gestion de certificats OpenSSL,
+#de clés et d’autres fichiers.
+# * req *: Cette sous-commande indique que nous souhaitons utiliser la gestion de la demande de signature de certificat X.509.
+#«X.509» est une norme d’infrastructure à clé publique à laquelle SSL et TLS adhèrent pour la gestion de ses clés
+# et de ses certificats.
+#* -nodes *: Ceci indique à OpenSSL de sauter l’option permettant de sécuriser notre certificat
+#avec une phrase secrète. Nginx doit pouvoir lire le fichier, sans intervention de l’utilisateur,
+# au démarrage du serveur. Un mot de passe empêcherait cela, car nous devions le saisir après chaque redémarrage.
+#* -days 365 *: Cette option définit la durée pendant laquelle le certificat sera considéré comme valide.
+#Nous le fixons pour un an ici.
+#* -newkey rsa: 2048 *: Ceci spécifie que nous voulons générer un nouveau certificat et une nouvelle clé en même temps.
+# Nous n’avons pas créé la clé requise pour signer le certificat lors d’une étape précédente. Nous devons donc le créer avec
+# le certificat. La partie + rsa: 2048 + lui dit de créer une clé RSA de 2048 bits.
+# * -keyout *: Cette ligne indique à OpenSSL où placer le fichier de clé privée généré que nous sommes en train de créer.
+# * -out *: Ceci indique à OpenSSL où placer le certificat que nous créons.
+# Nous voulons créer un nouveau certificat X.509, nous utilisons donc cette sous-commande.
 
 service nginx start
+service php7.3-fpm start
+service mysql start
+mysql -uroot < /etc/mysql.txt
+service php7.3-fpm restart
 sleep infinity
---
+++ src/wp-config.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'BDD_Marion' );
+
+/** MySQL database username */
+define( 'DB_USER', 'Marion' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'mdp' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+# genere avec https://api.wordpress.org/secret-key/1.1/salt/
+define('AUTH_KEY',         '~?}`Yk=ody5zBM[jl2#-H-+e3YM^v5Y5_o8YoN(_CTVOLGK%X~|iPfti0!!_++p/');
+define('SECURE_AUTH_KEY',  'y:c%|n|z6eGDpA]+2k+qdg+W])o~+BF~S(N_eN=unZ8)~NGsUeOi,r}+u=#`Z&B-');
+define('LOGGED_IN_KEY',    'ica1M<&+84RxDh>oDOq|BKs8+Qdd^RFj]Fn jdq3~FsdRnlfXn{?D0@#FtX+S+o%');
+define('NONCE_KEY',        '^_*R1F23.-]@pq&C`-#gGx.YSj^pTo04<|*|p}cE1}01ZLI)[z!]xZ)<|2]J`MQD');
+define('AUTH_SALT',        'm%W2u.kkB;=_Br%CC2^s3V|/yH3e(#Yg53Kg~){q8E`m(+QUvh0|WM>E&tG|FT:P');
+define('SECURE_AUTH_SALT', '$ar|:gL*> #|n?CLUgZqMKGIQ`-_GKv)ddBrKl8tba0MQMa-GblBg8u;iW9Gg8c3');
+define('LOGGED_IN_SALT',   ' X_0YR3WeT=-A1-;E}%Mkh:3:vP^[ss.K$;Gp (}k=me22JM]yIGl6U?j+Z9-fvr');
+define('NONCE_SALT',       'jheTGq~sH&7,vc +uW@niTV&dm4s?iSfZ^j=|]9~ZeA*&uj2?q8ZADr&G^_L~]#w');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
